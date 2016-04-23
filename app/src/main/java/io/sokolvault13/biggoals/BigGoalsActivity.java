ad09a8b2728ee1e06a241bf05e044577bb589db1
Dmitry Sokolov <dmitry.apollo@ya.ru>
2016-04-23 21:23:05 +0300

    Progress bar for goals were added.

--- app/src/main/java/io/sokolvault13/biggoals/BigGoalsActivity.java
+++ app/src/main/java/io/sokolvault13/biggoals/BigGoalsActivity.java
@@ -3,35 +3,50 @@ package io.sokolvault13.biggoals;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
 
-import com.j256.ormlite.dao.Dao;
+import com.daimajia.numberprogressbar.NumberProgressBar;
 
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Iterator;
-
-import io.sokolvault13.biggoals.Model.BigGoal;
-import io.sokolvault13.biggoals.Model.ObjectiveType;
-import io.sokolvault13.biggoals.Model.SubGoal;
-import io.sokolvault13.biggoals.Model.Task;
 import io.sokolvault13.biggoals.db.HelperFactory;
-import io.sokolvault13.biggoals.Model.DatabaseHelper;
-
-import static io.sokolvault13.biggoals.IntentionDAOHelper.*;
 
 public class BigGoalsActivity extends AppCompatActivity {
+
+    private NumberProgressBar mProgressBar;
+    private int mProgessStatus = 0;
+    private Button mIncreaseButton;
+    private Button mDecreaseButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_big_goals);
+
+        mProgressBar = (NumberProgressBar) findViewById(R.id.progressBar);
+        mIncreaseButton = (Button) findViewById(R.id.increaseButton);
+        mDecreaseButton = (Button) findViewById(R.id.decreaseButton);
+
+        mIncreaseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mProgressBar.incrementProgressBy(1);
+            }
+        });
+
+        mDecreaseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mProgressBar.incrementProgressBy(-1);
+            }
+        });
     }
 
     @Override
     protected void onStart() {
         super.onStart();
         HelperFactory.setHelper(getApplicationContext());
+        mProgressBar.setProgress(10);
     }
 
     @Override
